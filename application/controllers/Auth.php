52d2e71bf35f7010795cc5133e4b46d3d46c3f37
Arif Wicaksono <arif.wicaksono@bps.go.id>
2020-08-04 13:11:33 +0900

    Update SSO 1.3

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -160,14 +160,15 @@ class Auth extends MY_Controller
     public function logout()
     {
 
-    require $_SERVER['DOCUMENT_ROOT'].'/spk/application/controllers/vendor/autoload.php';
-    require $_SERVER['DOCUMENT_ROOT'].'/spk/application/controllers/src/Provider/Keycloak.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/assets/vendor/autoload.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/application/controllers/src/Provider/Keycloak.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/application/controllers/src/Provider/KeycloakResourceOwner.php';   
     $provider = new IrsadArief\OAuth2\Client\Provider\Keycloak([
         'authServerUrl'         => 'https://sso.bps.go.id',
         'realm'                 => 'pegawai-bps',
         'clientId'              => '19105-spk-4r1',
         'clientSecret'          => 'fbe2606f-b543-41db-98db-a19f13229932',
-        'redirectUri'           => 'http://localhost/spk'
+        'redirectUri'           => 'https://webapps.bps.go.id/manokwarikab/'
     ]);
 
         $this->session->sess_destroy();
