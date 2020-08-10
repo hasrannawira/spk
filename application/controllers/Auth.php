75650a38ceaab20b0e3afac62c4626409d730a5b
Arif Wicaksono <arif.wicaksono@bps.go.id>
2020-08-10 18:08:11 +0900

    KCA Beta 1.0

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -16,9 +16,9 @@ class Auth extends MY_Controller
     {
     // var_dump(file_exists(require base_url('application/controllers/vendor/autoload.php'));
     // session_start();
-    require $_SERVER['DOCUMENT_ROOT'].'/spk/application/controllers/vendor/autoload.php';
-    require $_SERVER['DOCUMENT_ROOT'].'/spk/application/controllers/src/Provider/Keycloak.php';
-    require $_SERVER['DOCUMENT_ROOT'].'/spk/application/controllers/src/Provider/KeycloakResourceOwner.php';  
+    require $_SERVER['DOCUMENT_ROOT'].'/assets/vendor/autoload.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/application/controllers/src/Provider/Keycloak.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/application/controllers/src/Provider/KeycloakResourceOwner.php';  
 
     $provider = new IrsadArief\OAuth2\Client\Provider\Keycloak([
         'authServerUrl'         => 'https://sso.bps.go.id',
@@ -144,8 +144,9 @@ class Auth extends MY_Controller
     public function logout()
     {
 
-    require $_SERVER['DOCUMENT_ROOT'].'/spk/application/controllers/vendor/autoload.php';
-    require $_SERVER['DOCUMENT_ROOT'].'/spk/application/controllers/src/Provider/Keycloak.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/assets/vendor/autoload.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/application/controllers/src/Provider/Keycloak.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/application/controllers/src/Provider/KeycloakResourceOwner.php';
     $provider = new IrsadArief\OAuth2\Client\Provider\Keycloak([
         'authServerUrl'         => 'https://sso.bps.go.id',
         'realm'                 => 'pegawai-bps',
