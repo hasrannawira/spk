bf620d7edb1c1bb7bdab33d3bed7163d1f4b9ad9
hasrannawira <hasrannawira9@gmail.com>
2024-07-09 13:27:22 +0900

    Update _header.php
    coba aja

--- application/views/layouts/member/_header.php
+++ application/views/layouts/member/_header.php
@@ -4,7 +4,7 @@
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini"><small><b>SPK</b></small></span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>SPK BPS</b></span>
+    <span class="logo-lg"><b>SPK BPS MANOKWARI</b></span>
   </a>
   <!-- nav -->
   <?php require_once('_nav.php') ;?>
